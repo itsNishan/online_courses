7a0baf177b82f8d4b118d4f6b03323372705d25d
ArunMajhi <50703960+ArunMajhi@users.noreply.github.com>
2021-01-24 22:35:03 +0545

    Inital Setup for Front-end

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 			var myapp = new express();
 			
+			// ejs
+		myapp.set('views', __dirname + '/views');
+		myapp.set('view engine', 'ejs');
 			
 			// set port
 		myapp.listen(3000);
