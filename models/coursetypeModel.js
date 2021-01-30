fdd6c3f93dd676412444cfd50c58b0895d5821ee
ArunMajhi <50703960+ArunMajhi@users.noreply.github.com>
2021-01-30 20:47:16 +0545

    coursetypeModel setup

---
+++ models/coursetypeModel.js
@@ -0,0 +1,29 @@
+var mysequelize = require('../configs/dbconfigs')
+const myUsers = mysequelize.sequelize.define('myUsers',
+{
+
+	id: {
+		type: mysequelize.Sequelize.BIGINT,
+		primaryKey: true,
+		autoIncrement: true,
+		allowNull : false
+	},
+
+
+	coursetype_title: {
+		type: mysequelize.Sequelize.STRING,
+		allowNull : false
+	}
+
+},
+
+{
+	freezeTableName : true,
+	tableName: 'coursetype'
+}
+
+)
+
+myUsers.sync({force:false})
+
+
