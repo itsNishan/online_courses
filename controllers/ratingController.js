42b4a6c16bd631db3a6a14509431c9e19d1f7097
sujan012 <devilhunter.suzan@gmail.com>
2021-02-04 11:57:36 +0545

    RatingControllerUpdated

--- controllers/ratingController.js
+++ controllers/ratingController.js
@@ -23,3 +23,67 @@ function ratingRegister(req, res, next) {
             next({ "status": 500, "message": "DB Error" });
         })
 }
+
+
+//Rating data update
+function ratingUpdate(req, res, next) {
+    if (req.body.id != '') {
+        ratingmodel.update({
+            rating: req.body.Rating,
+            courseID: req.body.CourseID,
+            studentID: req.body.StudentID
+    
+            }, {
+                where: { id: req.params.id }
+            })
+            .then(function(result) {
+                next();
+            })
+            .catch(function(err) {
+                next({ "status":500, "message": "DB Error" });
+            })
+        } else {
+            next({ "status": 500, "message":"Invalid Rating data" });
+    
+        }
+              }
+    
+    
+    
+    
+    // token
+    function token(req, res, next) {
+        jwt.sign({ username: req.body.username, accesslevel: 'superadmin' }, 'thisissecretkey', { expiresIn: '10h' },
+            function(err, token) {
+                // console.log(token);
+                if (err != null || undefined) {
+                    console.log(err);
+                    res.send({ "status": "401", "message": "unauthorized" });
+                } else {
+                    req.genToken = token;
+                    // res.status(200);
+                    // res.json(token);
+                    next();
+                    console.log(token);
+                }
+            });
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    module.exports = {
+        ratingRegister,
+     
+        ratingUpdate,
+    
+           token,
+    
+    
+    }
+    
